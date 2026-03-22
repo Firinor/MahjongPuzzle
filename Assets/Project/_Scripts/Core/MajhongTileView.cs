983d4110e2ff84d4290a87dbbc8050da72e6dcd1
taraszachesov <tarmz@rambler.ru>
2026-03-22 16:45:47 +0300

    Selection

--- Assets/Project/_Scripts/Core/MajhongTileView.cs
+++ Assets/Project/_Scripts/Core/MajhongTileView.cs
@@ -5,11 +5,18 @@ public class MajhongTileView : MonoBehaviour
 {
     [SerializeField] 
     private SpriteRenderer Renderer;
+    [SerializeField] 
+    private MeshRenderer Cube;
     private Tile Data;
 
     public void SetData(Tile data)
     {
         Data = data;
         Renderer.sprite = data.Sprite;
+    }
+
+    public void SetMaterial(Material material)
+    {
+        Cube.material = material;
     }
 }
---
+++ Assets/Project/_Scripts/Core/PlayerHand.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+
+public class PlayerHand : MonoBehaviour
+{
+    [SerializeField] 
+    private Material defaultMaterial;
+    [SerializeField] 
+    private Material selectedMaterial;
+
+    private MajhongTileView tile;
+    private InputActionAsset action;
+
+    private void Start()
+    {
+        action = InputSystem.actions;
+        EnhancedTouchSupport.Enable();
+        action.FindAction("Click").performed += FindTile;
+    }
+
+    private void FindTile(InputAction.CallbackContext obj)
+    {
+        if (!obj.control.IsPressed())
+            return;
+
+        WashHand();
+
+        MajhongTileView tile = GetRayHitTile();
+        
+        if(tile == null)
+            return;
+
+        this.tile = tile;
+        tile.SetMaterial(selectedMaterial);
+    }
+
+    private void WashHand()
+    {
+        if (tile != null)
+        {
+            tile.SetMaterial(defaultMaterial);
+        }
+        
+        tile = null;
+    }
+
+    private static MajhongTileView GetRayHitTile()
+    {
+        Vector2 position;
+        if (Touch.activeTouches.Count > 0)
+            position = Touch.activeTouches[0].screenPosition;
+        else
+            position = Mouse.current.position.ReadValue();
+
+        Ray ray = Camera.main.ScreenPointToRay(position);
+
+        MajhongTileView result = null;
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            hit.collider.TryGetComponent(out result);
+        }
+        return result;
+    }
+}
