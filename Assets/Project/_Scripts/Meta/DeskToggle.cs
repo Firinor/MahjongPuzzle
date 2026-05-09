e91e48e9ccb4701dc97d7945b8c65428a4c1a332
taraszachesov <tarmz@rambler.ru>
2026-05-10 00:05:15 +0300

    Meta music done

--- Assets/Project/_Scripts/Meta/DeskToggle.cs
+++ Assets/Project/_Scripts/Meta/DeskToggle.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class DeskToggle : MonoBehaviour
 {
     public string ID;
+    public Image ImageBackground;
     public Image Image;
     public Button Button;
     public Image Checkmark;
@@ -11,12 +12,14 @@ public class DeskToggle : MonoBehaviour
 
     public void Unlock(Sprite image)
     {
+        ImageBackground.enabled = true;
         Image.enabled = true;
         Image.sprite = image;
         UnlockButton.SetActive(false);
     }
     public void Lock()
     {
+        ImageBackground.enabled = false;
         Image.enabled = false;
         UnlockButton.SetActive(true);
     }
