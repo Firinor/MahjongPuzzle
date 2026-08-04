5b15a2aa0c4ec480ebc1e71bef756e1249b8b462
taraszachesov <tarmz@rambler.ru>
2026-08-04 21:43:58 +0300

    Score adaptive UI

--- Assets/Project/_Scripts/Application/CanvasRatioManager.cs
+++ Assets/Project/_Scripts/Application/CanvasRatioManager.cs
@@ -1,21 +1,40 @@
+using System;
 using UnityEngine;
 
 public class CanvasRatioManager : MonoBehaviour
 {
+    public event Action<ScreenOrientation> onChange;
+#if UNITY_EDITOR
+    public bool isDebug;
+#endif
+    
+    private bool isLandscape = true;
+    
     void Start()
     {
         if (!SystemInfo.supportsGyroscope) 
             Destroy(gameObject);
-        
-        ApplyOrientation(Screen.orientation);
     }
-    /*void OnOrientationChange()
+
+    private void Update()
     {
-        ApplyOrientation(Screen.orientation);
-    }*/
-    
-    private void ApplyOrientation(ScreenOrientation orientation)
-    {
-       
+        if (isLandscape && Screen.height > Screen.width)
+        {
+            isLandscape = false;
+#if UNITY_EDITOR
+            if(isDebug)
+                Debug.Log(Screen.orientation);
+#endif
+            onChange?.Invoke(Screen.orientation);
+        }
+        if(!isLandscape && Screen.height < Screen.width)
+        {
+            isLandscape = true;
+#if UNITY_EDITOR
+            if(isDebug)
+                Debug.Log(Screen.orientation);
+#endif
+            onChange?.Invoke(Screen.orientation);
+        }
     }
 }
